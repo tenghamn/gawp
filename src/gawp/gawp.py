dbc8fa9934a7d8358bbaa96e30856866d5ee402f
Johan Tenghamn <johan.tenghamn@gmail.com>
2025-02-04 09:57:07 +0100

    Update zooming and documentation

--- src/gawp/gawp.py
+++ src/gawp/gawp.py
@@ -3,18 +3,6 @@ from gawp.html import create_html_with_image
 from gawp.types_and_default_values import ImageTypes, DEFAULT_CMAP
 from IPython.display import display, HTML
 
-html = """
-<html>
-    <head>
-        <title>Test Page</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <p>This is a test page.</p>
-    </body>
-</html>
-"""
-
 
 def in_web(image: ImageTypes, cmap: str = DEFAULT_CMAP):
     show_html_in_browser(create_html_with_image(image, cmap))
